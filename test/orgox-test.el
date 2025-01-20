c3d9a2cf4840481b310852f035efb991a68da9c7
Pieter Swinkels <swinkels.pieter@yahoo.com>
2025-01-20 23:36:21 +0100

    Unit test creation of ox-hugo file from note

--- test/orgox-test.el
+++ test/orgox-test.el
@@ -1,14 +1,9 @@
 ;;; orgox-test.el --- Tests for orgox
 
-(defun extract-date (buffer-name)
-  (if (string-match-p "^20[[:digit:]]\\{6\\}$" (file-name-sans-extension buffer-name))
-      (concat
-       (substring buffer-name 0 4)
-       "-"
-       (substring buffer-name 4 6)
-       "-"
-       (substring buffer-name 6 8))
-    "Unable to extract date"))
+(require 'f)
+(require 'orgox)
+
+(defvar test-directory (file-name-concat default-directory "test"))
 
 (ert-deftest test-extract-date()
   (should (string= (extract-date "20250111.org") "2025-01-11"))
@@ -17,5 +12,22 @@
   ;; anything else, it will return a string that indicates an error
   (should (string= (extract-date "abcdefgh.org") "Unable to extract date"))
   (should (string= (extract-date "*Messages*") "Unable to extract date")))
+
+(ert-deftest test-orgox-current-buffer-to-ox-hugo()
+  (let ((org-file (file-name-concat test-directory "20250119.org"))
+        (expected-ox-hugo-org-file (file-name-concat test-directory "20250119.ox-hugo.org"))
+        (ox-hugo-org-file (file-name-concat temporary-file-directory "20250119.ox-hugo.org")))
+
+    ;; delete any pre-existing output file - this is a very poor-mans solution
+    ;; to a teardown phase
+    (when (file-exists-p ox-hugo-org-file)
+      (delete-file ox-hugo-org-file))
+
+    (with-current-buffer (find-file-noselect org-file)
+      (let ((orgox-content-org-directory temporary-file-directory))
+        (orgox-current-buffer-to-ox-hugo)))
+
+    (should (file-readable-p ox-hugo-org-file))
+    (should (string= (f-read expected-ox-hugo-org-file) (f-read ox-hugo-org-file)))))
 
 ;;; orgox-test.el ends here
