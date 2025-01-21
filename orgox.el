c6feb61f43a6221ba2cbcd64e5c71cdfd19b5b39
Pieter Swinkels <swinkels.pieter@yahoo.com>
2025-01-21 22:35:54 +0100

    Reorder orgox.el to have public functions first

--- orgox.el
+++ orgox.el
@@ -43,28 +43,23 @@
   "Directory of ox-hugo site to hold org-files to publish."
   :type 'directory :group 'orgox)
 
-(defun slug-for-current-heading ()
-  (let ((heading (nth 4 (org-heading-components))))
-    (string-replace " " "-" (downcase heading))))
+(defun orgox-publish-current-buffer ()
+  (interactive)
+  (let ((ox-hugo-file (orgox-current-buffer-to-ox-hugo)))
+    (with-current-buffer (find-file-noselect ox-hugo-file)
+      (org-hugo-export-wim-to-md :all-subtrees))))
 
-(defun extract-date (buffer-name)
-  (let ((elements (extract-date-elements buffer-name)))
-    (format "%s-%s-%s" (nth 0 elements) (nth 1 elements) (nth 2 elements))))
+(defun orgox-current-buffer-to-ox-hugo ()
+  (interactive)
+  (write-as-ox-hugo-file (current-buffer) orgox-content-org-directory))
 
-(defun extract-date-sections (buffer-name)
-  (let ((elements (extract-date-elements buffer-name)))
-    (format "posts/%s/%s/%s" (nth 0 elements) (nth 1 elements) (nth 2 elements))))
-
-(defun extract-date-elements (buffer-name)
-  (if (string-match-p "^20[[:digit:]]\\{6\\}$" (file-name-sans-extension buffer-name))
-      (list
-       (substring buffer-name 0 4)
-       (substring buffer-name 4 6)
-       (substring buffer-name 6 8))
-    (error (format "Unable to extract date elements from \"%s\"" buffer-name))))
-
-(defun get-ox-hugo-file-name (org-buffer-name)
-  (concat (file-name-sans-extension org-buffer-name) ".ox-hugo.org"))
+(defun write-as-ox-hugo-file (org-buffer dest-dir)
+  (let* ((ox-hugo-file-name (get-ox-hugo-file-name (buffer-name org-buffer)))
+         (ox-hugo-file (file-name-concat dest-dir ox-hugo-file-name)))
+    (with-current-buffer (find-file-noselect ox-hugo-file)
+      (write-as-ox-hugo-buffer org-buffer (current-buffer))
+      (save-buffer))
+    ox-hugo-file))
 
 (defun write-as-ox-hugo-buffer (org-buffer ox-hugo-buffer)
   (let ((org-buffer-name (buffer-name org-buffer)))
@@ -82,22 +77,27 @@
       (org-set-property "EXPORT_FILE_NAME" (slug-for-current-heading))
       (org-set-property "EXPORT_DATE" (extract-date org-buffer-name)))))
 
-(defun write-as-ox-hugo-file (org-buffer dest-dir)
-  (let* ((ox-hugo-file-name (get-ox-hugo-file-name (buffer-name org-buffer)))
-         (ox-hugo-file (file-name-concat dest-dir ox-hugo-file-name)))
-    (with-current-buffer (find-file-noselect ox-hugo-file)
-      (write-as-ox-hugo-buffer org-buffer (current-buffer))
-      (save-buffer))
-    ox-hugo-file))
+(defun extract-date-sections (buffer-name)
+  (let ((elements (extract-date-elements buffer-name)))
+    (format "posts/%s/%s/%s" (nth 0 elements) (nth 1 elements) (nth 2 elements))))
 
-(defun orgox-current-buffer-to-ox-hugo ()
-  (interactive)
-  (write-as-ox-hugo-file (current-buffer) orgox-content-org-directory))
+(defun extract-date-elements (buffer-name)
+  (if (string-match-p "^20[[:digit:]]\\{6\\}$" (file-name-sans-extension buffer-name))
+      (list
+       (substring buffer-name 0 4)
+       (substring buffer-name 4 6)
+       (substring buffer-name 6 8))
+    (error (format "Unable to extract date elements from \"%s\"" buffer-name))))
 
-(defun orgox-publish-current-buffer ()
-  (interactive)
-  (let ((ox-hugo-file (orgox-current-buffer-to-ox-hugo)))
-    (with-current-buffer (find-file-noselect ox-hugo-file)
-      (org-hugo-export-wim-to-md :all-subtrees))))
+(defun extract-date (buffer-name)
+  (let ((elements (extract-date-elements buffer-name)))
+    (format "%s-%s-%s" (nth 0 elements) (nth 1 elements) (nth 2 elements))))
+
+(defun get-ox-hugo-file-name (org-buffer-name)
+  (concat (file-name-sans-extension org-buffer-name) ".ox-hugo.org"))
+
+(defun slug-for-current-heading ()
+  (let ((heading (nth 4 (org-heading-components))))
+    (string-replace " " "-" (downcase heading))))
 
 ;;; orgox.el ends here
